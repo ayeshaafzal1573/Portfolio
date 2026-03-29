bd714e0628715866c39a674b29eb5054d4d8c0ee
Ayesha Afzal <ayeshaafzal1573@gmail.com>
2026-03-29 16:36:57 +0500

    added microsoft clarify

--- app/layout.tsx
+++ app/layout.tsx
@@ -107,8 +107,21 @@ export default function RootLayout({
               name: "Ayesha Afzal",
               jobTitle: "Full-Stack & Mobile App Developer",
               url: "https://ayeshaafzalqadir.vercel.app",
-              image: "https://ayeshaafzalqadir.vercel.app/ayesha-afzal-qadir.jpg",
+              image: "https://ayeshaafzalqadir.vercel.app/ayesha-afzal-qadir.jpeg",
             }),
+          }}
+        />
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "w3bm5vdtqa");
+            `,
           }}
         />
       </head>
